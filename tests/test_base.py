ed7faa7f62fb5c8d21ffc5e6699ca47f25f49c97
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-05-13 23:45:37 -0400

    change test

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,14 +2,19 @@ import jar
 
 
 class HelloWorld(jar.JarBase):
+
+    base_image = "python:3.7"
+
     def setup_image(self):
-        pass
+        self.RUN("python3 -m pip install numpy")
 
     def entrypoint(self):
-        print("hello world!")
+        print("hello world")
+        import numpy as np
+
+        print(f"numpy version {np.__version__}")
 
 
 def test_base():
 
-    helloworld = HelloWorld("python:3.7")
-    helloworld.save()
+    helloworld = HelloWorld()
