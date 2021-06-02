9ad916b6a97404a8b009586ba52f304ee90a9865
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-06-02 19:32:10 -0400

    add trace

--- mason/base.py
+++ mason/base.py
@@ -2,33 +2,9 @@ import os
 import inspect
 from typing import Optional
 from .client import get_docker_client, login, push
+from .trace import get_main, INDENT
 
 __all__ = ["Jar"]
-
-INDENT = 4
-
-
-def get_main(src, argspec):
-    ln = []
-    ln.append(src)
-    ln.append("if __name__ == '__main__':")
-    ln.append(" " * INDENT + "import argparse")
-    ln.append(" " * INDENT + "parser = argparse.ArgumentParser()")
-    for arg, typ in argspec.annotations.items():
-        if arg == "self":
-            continue
-        if typ in (list, tuple):
-            nargs = "+"
-            typ = typ.__args__[0]
-        else:
-            nargs = "1"
-        ln.append(
-            " " * INDENT
-            + f"parser.add_argument('--{arg}', type={typ.__name__}, nargs={nargs})"
-        )
-    ln.append(" " * INDENT + "kwargs = vars(parser.parse_args())")
-    ln.append(" " * INDENT + "main(**kwargs)")
-    return "\n".join(ln)
 
 
 class Jar:
