93d75eb9be77896ed9197df6f46b3fd8c522f650
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-06-03 15:36:40 -0400

    nip

--- mason/trace.py
+++ mason/trace.py
@@ -51,6 +51,7 @@ def get_main_source_file(src: str, argspec: NamedTuple) -> str:
         assert (
             arg in argspec.annotations
         ), f"Arg {arg} is not annotated. All entrypoint args should be annotated kwargs."
+
     for arg, typ in argspec.annotations.items():
         if arg in ("self", "cls"):
             continue
