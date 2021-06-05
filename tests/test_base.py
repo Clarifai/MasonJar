87d36c4cfeb6c785a851b9580ad16ee977a2cf45
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-06-05 15:16:07 -0400

    add tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,5 @@
 import mason
+import tempfile
 
 
 class HelloWorld(mason.Jar):
@@ -12,8 +13,78 @@ class HelloWorld(mason.Jar):
         print("hello world")
 
 
-def test_base():
+class HelloConstants(HelloWorld):
+    def constants(self):
+        self.a = 0
+        self.b = "1"
+        self.c = 2.0
+        self.d = [0, 1, 2, 3]
+
+
+class HelloHelpers(HelloWorld):
+    @mason.include
+    def a_helper_func(self, a):
+        print("hello world", a)
+
+    @classmethod
+    @mason.include
+    def class_helper(cls, b):
+        print("hello world", b)
+
+    @staticmethod
+    @mason.include
+    def static_helper(c):
+        print("hello world", c)
+
+
+def test_jar_files():
 
     hello = HelloWorld()
-    print(hello.dockerfile)
-    print(hello.main_file_source)
+    hello()
+
+    dockerfile = hello.dockerfile
+    assert "FROM python:3.7" in dockerfile
+    assert "RUN python3 -m pip install numpy" in dockerfile
+    mainfile = hello.main_file_source
+    assert 'print("hello world")' in mainfile
+
+
+def test_save():
+
+    with tempfile.TemporaryDirectory() as td:
+        hello = HelloWorld(root=td)
+        hello.save()
+
+
+def test_registry():
+
+    assert mason.Jar.registry is None
+
+    hello = HelloWorld()
+    hello.registry = "my.registry"
+
+    assert hello.registry == "my.registry"
+    assert mason.Jar.registry is None
+
+    HelloWorld.registry = "another.registry"
+    assert hello.registry == "my.registry"
+
+
+def test_constants():
+
+    hello = HelloConstants()
+    assert hello.a == hello._constant_registry["a"] == 0
+    assert hello.b == hello._constant_registry["b"] == "1"
+    assert hello.c == hello._constant_registry["c"] == 2.0
+    assert hello.d == hello._constant_registry["d"] == [0, 1, 2, 3]
+    hello.e = dict(a=1, b=2)
+    assert hello.e == hello._constant_registry["e"] == dict(a=1, b=2)
+
+
+def test_helpers():
+
+    hello = HelloHelpers()
+
+    assert "a_helper_func" in hello._helper_registry
+    assert "static_helper" in hello._helper_registry
+    assert "class_helper" in hello._helper_registry
