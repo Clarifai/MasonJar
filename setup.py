4b28973a847eda3fd61b3fe41eaa8d15782eaf98
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-06-16 18:23:02 -0400

    add classifier

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+
 from mason import __version__
 
 # load readme
@@ -8,10 +9,23 @@ with open("README.md", "r") as f:
 setup(
     name="mason-jar",
     version=__version__,
-    description="Pickle your container as a python class.",
+    description="Pickle your containerized experiment as a python class.",
     packages=find_packages(exclude=["tests"]),
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=["docker"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="docker container experiment",
     license="MIT",
 )
